50e07a0ef433ed6b3531e591b5bb43a0dd1345eb
junho <yunjunhojj@gmail.com>
2024-04-07 21:04:33 +0900

    ch3 예시 코드 추가

--- ch3/server.js
+++ ch3/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const api = require("./routes/api");
+
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+
+app.use("/api", api);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
